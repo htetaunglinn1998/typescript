0850fabbfeecede35681d022c7c72863798fa73f
htetaunglinn1998 <htetaung.linn@smilaxglobal.com>
2022-11-14 16:46:50 +0630

    Basics done

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-function add(n1, n2, showResult, resultPhrase) {
-    var result = n1 + n2;
-    if (showResult)
-        console.log(resultPhrase + result);
-    else
-        return result;
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is";
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: "Htet Aung Linn",
+    age: 24
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,12 +1,6 @@
-function add (n1:number,n2:number, showResult: boolean, resultPhrase:string){
-    const result = n1 + n2;
-    if(showResult) console.log(resultPhrase+result)
-    else return result;
+const people: {name: string; age: number;} = {
+    name : "Htet Aung Linn",
+    age:24
 }
 
-const number1 =5;
-const number2 =2.8;
-const printResult = true;
-const resultPhrase = "Result is";
-
-add(number1, number2, printResult, resultPhrase)
+console.log(people.name)
