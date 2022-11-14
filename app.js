0416812402098ed5f3e868aef408977c66c8f523
htetaunglinn1998 <htetaung.linn@smilaxglobal.com>
2022-11-14 15:27:46 +0630

    Type number string boolean

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Your codes go here");
+function add(n1, n2, showResult, resultPhrase) {
+    var result = n1 + n2;
+    if (showResult)
+        console.log(resultPhrase + result);
+    else
+        return result;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is";
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Your codes go here")
+function add (n1:number,n2:number, showResult: boolean, resultPhrase:string){
+    const result = n1 + n2;
+    if(showResult) console.log(resultPhrase+result)
+    else return result;
+}
+
+const number1 =5;
+const number2 =2.8;
+const printResult = true;
+const resultPhrase = "Result is";
+
+add(number1, number2, printResult, resultPhrase)
